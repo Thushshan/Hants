9e216b37f9d16a3054a2adfa58c1003686af34c4
Thushshan <46339176+Thushshan@users.noreply.github.com>
2019-01-04 11:38:08 +0530

    admin profile creation

--- students.php
+++ students.php
@@ -5,40 +5,26 @@
 							<div class="card mb-4 p-2">
 								<img class="card-img-top p-4" src="assets/images/student-add.png" alt="Image">
 								<div class="card-body">
-									<a href="student-add.php" class="btn btn-lg btn-dark btn-block">Add</a>
+									<a href="student-add.php" class="btn btn-lg btn-dark btn-block">New User</a>
 								</div>
 							</div>
 						</div>
 						<div class="col-md-3">
 							<div class="card mb-4 p-2">
-								<img class="card-img-top p-4" src="assets/images/student-search.png" alt="Image">
+								<img class="card-img-top p-4" src="assets/images/new location.png" alt="Image">
 								<div class="card-body">
-									<a href="student-search.php" class="btn btn-lg btn-dark btn-block">Search</a>
+									<a href="student-search.php" class="btn btn-lg btn-dark btn-block">New Location</a>
 								</div>
 							</div>
 						</div>
 						<div class="col-md-3">
 							<div class="card mb-4 p-2">
-								<img class="card-img-top p-4" src="assets/images/student-attendance.png" alt="Image">
+								<img class="card-img-top p-4" src="assets/images/new department.png" alt="Image">
 								<div class="card-body">
-									<a href="student-attendance.php" class="btn btn-lg btn-dark btn-block">Attendance</a>
+									<a href="student-attendance.php" class="btn btn-lg btn-dark btn-block">New Department</a>
 								</div>
 							</div>
-						</div>
-						<div class="col-md-3">
-							<div class="card mb-4 p-2">
-								<img class="card-img-top p-4" src="assets/images/businessman-overview.png" alt="Image">
-								<div class="card-body">
-									<a href="student-overview.php" class="btn btn-lg btn-dark btn-block">Overview</a>
-								</div>
-							</div>
-						</div>
-						<div class="col-md-3">
-							<div class="card mb-4 p-2">
-								<img class="card-img-top p-4" src="assets/images/student-reports.png" alt="Image">
-								<div class="card-body">
-									<a href="student-reports.php" class="btn btn-lg btn-dark btn-block">Reports</a>
-								</div>
+						
 							</div>
 						</div>
 					</div>
