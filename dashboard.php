dfa0c1bb6b3c289ba0751da034d56ad1fa0d3238
Thushshan <46339176+Thushshan@users.noreply.github.com>
2019-01-30 16:13:44 +0530

    commit

--- dashboard.php
+++ dashboard.php
@@ -79,7 +79,7 @@
 										<div class="row">
 											<div class="col-md-3">
 												<div class="card mb-4 p-2" style="background-color:#e2e8e5">
-													<img height="250" width="200" class="card-img-top p-4" src="assets/images/businessman.png" alt="Image">
+													<img height="250" width="200" class="card-img-top p-4" src="assets/images/User_creation.png" alt="Image">
 													<div class="card-body">
 														<a href="admin-staff-add.php" class="btn btn-lg btn-dark btn-block"> Manage Users </a>
 													</div>
@@ -95,7 +95,7 @@
 											</div>
 											<div class="col-md-3">
 												<div class="card mb-4 p-2" style="background-color:#e2e8e5">
-													<img height="250" width="200" class="card-img-top p-4" src="assets/images/businessman.png" alt="Image">
+													<img height="250" width="200" class="card-img-top p-4" src="assets/images/department_creation.png" alt="Image">
 													<div class="card-body">
 														<a href="admin-manage-departments.php" class="btn btn-lg btn-dark btn-block"> Departments </a>
 													</div>
